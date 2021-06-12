112ff513f9bc0c172c0d3ca71a022780704fd63f
Abhay <abhayonlyone@gmail.com>
2021-06-12 16:43:23 +0530

    Added carrier accout creation

---
+++ src/models/CarrierTypes.ts
@@ -0,0 +1,3 @@
+export class CarrierRegisterData {
+  username: string;
+}
--- src/routes/carrier-routes.ts
+++ src/routes/carrier-routes.ts
@@ -1,8 +1,50 @@
 import { Router } from "express";
 import { getConnection } from "typeorm";
+import { Carrier } from "../entity/Carrier";
 import { Message } from "../entity/Message";
+import { CarrierRegisterData } from "../models/CarrierTypes";
 import { SentMessagesData } from "../models/MessageID.model";
 const carrierRoutes = Router();
+
+carrierRoutes.post("/", async (req, res) => {
+  const data = req.body as CarrierRegisterData;
+  if (!data.username) {
+    res.status(400).json({
+      error: "No username field in request.",
+    });
+  }
+
+  const dbConnection = getConnection();
+  const repository = dbConnection.getRepository(Carrier);
+
+  let existingCarrier: Carrier | undefined;
+  try {
+    // where username = :username or email = :email
+    existingCarrier = await repository.findOne({
+      where: [{ username: data.username }],
+    });
+  } catch (err) {
+    res.sendStatus(500);
+  }
+
+  if (existingCarrier) {
+    res.statusCode = 400;
+
+    if (existingCarrier.username === data.username) {
+      res.json({ error: "Username already taken" });
+    }
+  }
+
+  const carrier = new Carrier();
+  carrier.username = data.username;
+
+  try {
+    const savedCarrier = await dbConnection.manager.save(carrier);
+    res.status(200).json({ id: savedCarrier.id });
+  } catch (err) {
+    res.sendStatus(500);
+  }
+});
 
 carrierRoutes.get("/messages", async (_, res) => {
   const dbConnection = getConnection();
